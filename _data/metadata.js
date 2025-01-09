ab333cb34045a01a30ceffdb4e7166ec7ef6bdd2
adamdjbrett <adamdjbrett@users.noreply.github.com>
2025-01-08 21:22:18 -0600

    Update metadata.js
    fix url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 export default {
 	title: "11ty Gets Tufte",
 	description: "A Tufte CSS Project built with 11ty aka Eleventy for your projects",
-	url: "000000076.xyz",
+	url: "https://tufte.000000076.xyz",
 	language: "en",
 	locale: "en_US",
 	image: "/img/possum.avif",
